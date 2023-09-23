1fd296cd75d17c37f58e0506816d92c9b834d586
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-09-24 01:17:58 +0530

    fixed bugs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,36 @@
 import React, { useState, useEffect } from "react";
 import "./Card.css";
 const Card = ({ elem, type }) => {
+  const genre = {
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Science Fiction",
+    10770: "TV Movie",
+    53: "Thriller",
+    10752: "War",
+    37: "Western",
+    10759: "Action & Adventure",
+    10762: "Kids",
+    10763: "News",
+    10764: "Reality",
+    10765: "Sci-Fi & Fantasy",
+    10766: "Soap",
+    10767: "Talk",
+    10768: "War & Politics",
+  };
+
   const [name, setName] = useState("");
 
   useEffect(() => {
@@ -33,7 +63,7 @@ const Card = ({ elem, type }) => {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkGOsNvcZKUcfBrsNSE0aNtXoxLaLmuIYfjh7sNsPlww&s"
               alt=""
             />
-            <p>{elem.vote_average}/10</p>
+            <p>{Number(elem.vote_average).toFixed(2)}/10</p>
           </div>
           <div className="rottenTomato">
             <img
@@ -44,7 +74,14 @@ const Card = ({ elem, type }) => {
           </div>
         </div>
         <div className="card-container-info-genre">
-          <p>Action Adventure Horror</p>
+          <p>
+            {elem &&
+              elem.genre_ids.map((item, index) => {
+                return elem.genre_ids.length === index + 1
+                  ? `${genre[item]}`
+                  : `${genre[item]}, `;
+              })}
+          </p>
         </div>
       </div>
     </div>
